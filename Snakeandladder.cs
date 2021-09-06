150a50b3de748ad0462fbf3873ccc3abd908f5fe
unknown <shaunak12wankhade@ggmail.com>
2021-09-06 16:45:09 +0530

    [Shaunak] Added single player at start position 0

---
+++ Snakeandladder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    class Snakeandladder
+    {
+        static void Main(string[] args)
+        {
+                //game with single player at start
+                int player = 1;
+                int StartingPosition = 0;
+                //computation
+                Console.WriteLine("No. of Player " + player);
+                Console.WriteLine("Position at start:- " + StartingPosition);
+                Console.ReadLine();
+            }
+    }
+}
