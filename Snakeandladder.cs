8c0c31d0a39b356f383fd95f1d9686137d643a4b
unknown <shaunak12wankhade@ggmail.com>
2021-10-09 13:18:12 +0530

    [Shaunak] "Added a check options"

--- Snakeandladder.cs
+++ Snakeandladder.cs
@@ -10,10 +10,28 @@ namespace Snake_And_Ladder
     {
         static void Main(string[] args)
         {
-            // UC2 Player rolls a die
-            Random random = new Random();
-            int dieRoll = random.Next(0, 7);
-            Console.WriteLine("You got " + dieRoll + " in die roll.");
+          
+            {
+                Random random = new Random();
+                int diceRoll = random.Next(1, 6);
+                Console.WriteLine("You got " + diceRoll);
+                //Constants
+                const int samePosition = 0;
+                const int forward = 1;
+                Random random1 = new Random();
+                int options = random1.Next(0, 2);
+                if (options == samePosition)
+                {
+                    Console.WriteLine("You Got No Option Stay in the Same Place.");
+                }
+                else if (options == forward)
+                {
+                    Console.WriteLine("You Got Ladder Your moving ahead by " + diceRoll + " numbers.");
+                }
+                else
+                    Console.WriteLine("You Got Snake Your Moving Backward by " + diceRoll + " numbers.");
+            }
+
         }
     }
 }
