3d2b407ef6ee28150240b120916a6c5b852c2fad
unknown <shaunak12wankhade@ggmail.com>
2021-10-10 11:08:41 +0530

    [Shaunak] Added :- Two players playing game

--- Snakeandladder.cs
+++ Snakeandladder.cs
@@ -10,85 +10,126 @@ namespace Snake_And_Ladder
     {
         static void Main(string[] args)
         {
-            Snakeandladder.nooftimedicerolled();
+            Snakeandladder.twoplayers();
             Console.ReadLine();
         }
-       
 
-            //Constants
-        public const int maxposition = 100;
-        public const int sameposition = 0;
+
+        //Constants
+        public const int maxPosition = 100;
+        public const int samePosition = 0;
         public const int forward = 1;
         public const int backward = 2;
-        public static void nooftimedicerolled()
+        public static void twoplayers()
         {
-            Console.WriteLine("");
+            Console.WriteLine("");             
 
-            //variables
-            int currentposition = 1;
-            int newposition = 0;
+            //Variables
+            int player1Position = 0;
+            int player2Position = 0;
+            int newPositionPlayer1 = 0;
+            int newPositionPlayer2 = 0;
             int noofdiceroll = 0;
 
-
-            while (currentposition <= maxposition)
+            //Computation
+            while (player1Position <= maxPosition && player2Position <= maxPosition)
             {
+                //Random numbers generation
                 Random random = new Random();
-                int dice = random.Next(1, 6);
-                Console.WriteLine("You got" + dice);
+                int dice1 = random.Next(1, 6);
+                int dice2 = random.Next(1, 6);
+                int options1 = random.Next(0, 3);
+                int options2 = random.Next(0, 3);
 
-                Random random1 = new Random();
-                int options = random1.Next(0, 3);
+                Console.WriteLine("Player 1 got " + dice1);
+                Console.WriteLine("Player 2 got " + dice2);
 
-                newposition = currentposition + dice;
+
+
+                newPositionPlayer1 = player1Position + dice1;
+                newPositionPlayer2 = player2Position + dice2;
                 noofdiceroll++;
 
-                if (newposition == 100)
+
+                if (newPositionPlayer1 == maxPosition)
                 {
-                    Console.WriteLine("You won");
-                    Console.WriteLine("Dice rolled" + noofdiceroll);
+                    Console.WriteLine("Player 1 won the Game :)");
+                    Console.WriteLine("Dice rolled " + noofdiceroll);
                     break;
                 }
-                else if (newposition > 100)
+
+                else if (newPositionPlayer1 > maxPosition)
                 {
-                    Console.WriteLine("You got No option so stay");
-                    Console.WriteLine("Your current position is: " + currentposition);
-                    Console.WriteLine("");
+                    Console.WriteLine("Player 1 got No option so stay");
+                    Console.WriteLine("Player 2 current Position is: " + player1Position);
+                    Console.WriteLine("");                    
                     continue;
-
                 }
 
-                if (options == sameposition)
+                if (newPositionPlayer2 == maxPosition)
                 {
-                    Console.WriteLine("You got no option so stay");
-                }
-                else if (options == forward)
-                {
-                    currentposition = currentposition + dice;
-                    Console.WriteLine("You got ladder so move forward" + dice);
-                }
-                else if (options == backward)
-                {
-                    currentposition = currentposition - dice;
-                    Console.WriteLine("You got snake so move backward" + dice);
-                }
-                if (currentposition >= 100)
-                {
-                    Console.WriteLine("You won");
+                    Console.WriteLine("Player 2 won the Game :)");
+                    Console.WriteLine("Dice rolled " + noofdiceroll);
                     break;
                 }
 
-                
-                else if (currentposition <= 0)
+                else if (newPositionPlayer2 > maxPosition)
                 {
-                    Console.WriteLine("You Lost so restart further");
-                    currentposition = currentposition - currentposition;
+                    Console.WriteLine("Player 2 got No option so stay");
+                    Console.WriteLine("Player 2 current Position is: " + player2Position);
+                    Console.WriteLine("-------------------------------------");                     
+                    continue;
                 }
-                Console.WriteLine("Your current position is : " + currentposition);
-                Console.WriteLine("");
+
+                //For Player 1
+                if (options1 == samePosition)
+                {
+                    Console.WriteLine("Player 1 got No option so Stay");
+                }
+                else if (options1 == forward)
+                {
+                    player1Position += dice1;
+                    Console.WriteLine("Player 1 got ladder so move forward " + dice1);
+                }
+                else if (options1 == backward)
+                {
+                    player1Position -= dice1;
+                    Console.WriteLine("Player 1 got snake so move backward " + dice1);
+                }
+
+                //For Player 2
+                if (options2 == samePosition)
+                {
+                    Console.WriteLine("Player 2 got No option so Stay");
+                }
+                else if (options2 == forward)
+                {
+                    player2Position += dice2;
+                    Console.WriteLine("Player 2 got ladder so move forward" + dice2);
+                }
+                else if (options2 == backward)
+                {
+                    player2Position -= dice2;
+                    Console.WriteLine("Player 2 got snake so move backward " + dice2);
+                }
+
+                if (player1Position <= 0)
+                {
+                    Console.WriteLine("Player 1 lost so restart further");
+                    player1Position = player1Position - player1Position;
+                }
+                if (player2Position <= 0)
+                {
+                    Console.WriteLine("Player 2 lost so restart further");
+                    player2Position = player2Position - player2Position;
+                }
+                Console.WriteLine("Player 1 current Position is: " + player1Position);
+                Console.WriteLine("Player 2 current Position is: " + player2Position);
+                Console.WriteLine("Dice rolled " + noofdiceroll);
+                Console.WriteLine("");                 //For separation
             }
-
-
         }
+        
         
     }
 }
